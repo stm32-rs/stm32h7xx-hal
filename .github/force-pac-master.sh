283c4b312eeee04c1e3bb33b61a948dc762ccd8d
Robert Jördens <rj@quartiq.de>
2019-07-03 21:08:58 +0200

    travis: tweak local pac crate installation

--- .github/force-pac-master.sh
+++ .github/force-pac-master.sh
@@ -5,13 +5,14 @@ set -e
 git clone https://github.com/stm32-rs/stm32-rs
 cd stm32-rs
 cargo install svd2rust
+# cargo install form  # not needed here
 rustup component add rustfmt
-pip install --user pyyaml
+pip3 install --user pyyaml
 cd svd
 ./extract.sh
 cd ..
-make patch
-make -j$(nproc) svd2rust
+make patch CRATES=stm32h7
+make -j$(nproc) svd2rust CRATES=stm32h7
 cd ..
 
 sed -i 's|/your/path/here/||' Cargo.toml
