b189688925aa456de4be3efdc3ca052445c97237
Jan Adä <jan561.github@gmail.com>
2019-12-22 17:10:25 +0100

    Added DmaExt to prelude

--- src/prelude.rs
+++ src/prelude.rs
@@ -3,6 +3,7 @@ pub use embedded_hal::prelude::*;
 
 pub use crate::adc::AdcExt as _stm32h7xx_hal_adc_AdcExt;
 pub use crate::delay::DelayExt as _stm32h7xx_hal_delay_DelayExt;
+pub use crate::dma::DmaExt as _stm32h7xx_hal_dma_DmaExt;
 pub use crate::flash::FlashExt as _stm32h7xx_hal_flash_FlashExt;
 pub use crate::gpio::GpioExt as _stm32h7xx_hal_gpio_GpioExt;
 pub use crate::i2c::I2cExt as _stm32h7xx_hal_i2c_I2cExt;
