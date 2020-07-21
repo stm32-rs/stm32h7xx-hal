8d6bf4a2614f9956d78146f574540cc88f93632d
Richard Meadows <962920+richardeoin@users.noreply.github.com>
2020-07-21 21:10:37 +0200

    Use mco2 output to check frequency of fractional-pll example

--- examples/fractional-pll.rs
+++ examples/fractional-pll.rs
@@ -10,7 +10,7 @@ use rtt_target::{rprintln, rtt_init_print};
 
 use cortex_m_rt::entry;
 use stm32h7xx_hal::rcc;
-use stm32h7xx_hal::{pac, prelude::*};
+use stm32h7xx_hal::{gpio::Speed, pac, prelude::*};
 
 #[entry]
 fn main() -> ! {
@@ -26,12 +26,19 @@ fn main() -> ! {
     println!("Setup RCC...                  ");
     let rcc = dp.RCC.constrain();
     let ccdr = rcc
+        .use_hse(25.mhz())
         .sys_ck(400.mhz())
-        .pll3_strategy(rcc::PllConfigStrategy::FractionalNotLess)
-        .pll3_p_ck((48_000 * 256).hz())
-        .pll3_q_ck((48_000 * 128).hz())
-        .pll3_r_ck((48_000 * 63).hz())
+        .pll2_strategy(rcc::PllConfigStrategy::FractionalNotLess)
+        .pll2_p_ck(12_288_000.hz())
+        .pll2_q_ck(6_144_000.hz())
+        .pll2_r_ck((48_000 * 63).hz())
+        // pll2_p / 2 --> mco2
+        .mco2_from_pll2_p_ck(7.mhz())
         .freeze(vos, &dp.SYSCFG);
+
+    // Enable MCO2 output pin
+    let gpioc = dp.GPIOC.split(ccdr.peripheral.GPIOC);
+    let _mco2_pin = gpioc.pc9.into_alternate_af0().set_speed(Speed::High);
 
     println!("");
     println!("stm32h7xx-hal example - Fractional PLL");
@@ -41,10 +48,11 @@ fn main() -> ! {
     println!("sys_ck = {} MHz", ccdr.clocks.sys_ck().0 as f32 / 1e6);
     assert_eq!(ccdr.clocks.sys_ck().0, 400_000_000);
 
-    println!("pll3_p_ck = {} MHz", ccdr.clocks.pll3_p_ck().unwrap().0);
-    println!("pll3_q_ck = {} MHz", ccdr.clocks.pll3_q_ck().unwrap().0);
-    println!("pll3_r_ck = {} MHz", ccdr.clocks.pll3_r_ck().unwrap().0);
-    // assert_eq!(ccdr.clocks.sys_ck().0, 100_000_000);
+    println!("pll2_p_ck = {} MHz", ccdr.clocks.pll2_p_ck().unwrap().0);
+    println!("pll2_q_ck = {} MHz", ccdr.clocks.pll2_q_ck().unwrap().0);
+    println!("pll2_r_ck = {} MHz", ccdr.clocks.pll2_r_ck().unwrap().0);
+
+    let _mco2_ck = ccdr.clocks.mco2_ck().unwrap().0;
 
     loop {}
 }
