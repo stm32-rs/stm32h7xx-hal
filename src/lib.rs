1a4b2a3479da6a3a216854c7e76f34491e9a1d41
Richard Meadows <962920+richardeoin@users.noreply.github.com>
2020-08-09 23:20:57 +0200

    Add new feature flags for rm0455 parts

--- src/lib.rs
+++ src/lib.rs
@@ -64,6 +64,9 @@ compile_error!(
         stm32h750v
         stm32h747cm7
         stm32h757cm7
+        stm32h7b3
+        stm32h7b0
+        stm32h7a3
 "
 );
 
@@ -99,6 +102,14 @@ pub use stm32h7::stm32h747cm7 as stm32;
 #[cfg(any(feature = "stm32h757cm7",))]
 pub use stm32h7::stm32h757cm7 as stm32;
 // TODO(rm0399): soundness of PeripheralREC macro in rcc/rec.rs
+
+// High Memory Integration
+#[cfg(any(
+    feature = "stm32h7b3",
+    feature = "stm32h7a3",
+    feature = "stm32h7b0",
+))]
+pub use stm32h7::stm32h7b3 as stm32;
 
 #[cfg(all(feature = "rm0433", feature = "rm0399"))]
 compile_error!("Cannot not select both rm0433 and rm0399");
