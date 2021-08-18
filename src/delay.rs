6691c7b5c5db398ab0b3cfb79c288967fa109df9
Richard Meadows <962920+richardeoin@users.noreply.github.com>
2021-08-18 20:52:22 +0200

    Test pedantic lint

--- src/delay.rs
+++ src/delay.rs
@@ -67,7 +67,7 @@ impl Delay {
     pub fn new(mut syst: SYST, clocks: CoreClocks) -> Self {
         syst.set_clock_source(SystClkSource::External);
 
-        Delay { clocks, syst }
+        Delay { syst, clocks }
     }
 
     /// Releases the system timer (SysTick) resource
