f1565d0347ebe3a000a0094c2dc0e4d588ed9f52
Richard Meadows <962920+richardeoin@users.noreply.github.com>
2022-11-03 22:06:15 +0100

    Fix description of ADC DMA example

--- examples/adc_dma.rs
+++ examples/adc_dma.rs
@@ -1,4 +1,4 @@
-//! Example of reading ADC1 data using DMA1/2
+//! Example of reading ADC1 data using DMA1
 //!
 //! For an example of using ADC3, see examples/temperature.rs
 //! For an example of using ADC1 and ADC2 together, see examples/adc12.rs
