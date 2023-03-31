9a9ee6d3a587815400165d70cd11cd63f678b574
James Logan <jlogan03@gmail.com>
2023-03-31 17:50:17 -0400

    remove added explicit feature flags for the 723,725,733 chips. remove dangling reference to nonexistent stm32h725 feature flag as well as the other added references.

--- src/lib.rs
+++ src/lib.rs
@@ -126,13 +126,7 @@ pub use stm32h7::stm32h757cm7 as stm32;
 pub use stm32h7::stm32h7b3 as stm32;
 
 // High Speed
-#[cfg(any(
-    feature = "stm32h725",
-    feature = "stm32h735",
-    feature = "stm32h730",
-    feature = "stm32h723",
-    feature = "stm32h733"
-))]
+#[cfg(feature = "stm32h735")]
 pub use stm32h7::stm32h735 as stm32;
 
 #[cfg(all(feature = "rm0433", feature = "rm0399"))]
