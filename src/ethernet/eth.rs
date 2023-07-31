3541cabc0c6c54d1542e503b14edff14caaadb26
ZachParallel <134434083+ZachParallel@users.noreply.github.com>
2023-07-31 10:41:48 -0700

    Explicit Device

--- src/ethernet/eth.rs
+++ src/ethernet/eth.rs
@@ -1027,7 +1027,7 @@ impl<'dma, 'tx> TxToken for EthTxToken<'dma, 'tx> {
 // }
 
 /// Use this Ethernet driver with [smoltcp](https://github.com/smoltcp-rs/smoltcp)
-impl<'a, 'rx, 'tx> Device for &'a mut EthernetDMA<'rx, 'tx> {
+impl<'a, 'rx, 'tx> phy::Device for &'a mut EthernetDMA<'rx, 'tx> {
     type RxToken<'token> = EthRxToken<'token, 'rx> where Self: 'token;
     type TxToken<'token> = EthTxToken<'token, 'tx> where Self: 'token;
 
