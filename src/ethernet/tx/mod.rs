91bf810f2235ebd24f876b3d64af7047ac3dac3c
ZachParallel <134434083+ZachParallel@users.noreply.github.com>
2023-08-01 08:38:49 -0700

    Change

--- src/ethernet/tx/mod.rs
+++ src/ethernet/tx/mod.rs
@@ -270,7 +270,7 @@ impl TxRing<'_> {
     fn entry_timestamp(&self, index: usize) -> Option<Timestamp> {
         let t = self.ring.descriptor(index);
         #[cfg(feature = "defmt")]
-        defmt::info!("Descriptor for index: {}", t.inner_raw);
+        defmt::info!("Descriptor for index: {}", t.inner_raw.desc);
         self.ring.descriptor(index).timestamp()
     }
 
