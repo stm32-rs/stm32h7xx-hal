d6d268f1b6854462a9cb34b5cd47cb417b2d8772
Harishguna Satgunarajah <hsatgunarajah@moveparallel.com>
2023-10-03 09:54:32 -0700

    fmt

--- src/ethernet/mod.rs
+++ src/ethernet/mod.rs
@@ -57,10 +57,10 @@ mod cache;
 pub(crate) use cache::Cache;
 
 mod eth;
-pub use eth::{eth_interrupt_handler, new, new_unchecked};
-pub use eth::{EthernetDMA, EthernetMAC, Parts};
 #[cfg(feature = "ptp")]
 pub use eth::PtpFrameWithId;
+pub use eth::{eth_interrupt_handler, new, new_unchecked};
+pub use eth::{EthernetDMA, EthernetMAC, Parts};
 
 /// Marks a set of pins used to communciate to a PHY with a Reduced Media
 /// Independent Interface (RMII)
