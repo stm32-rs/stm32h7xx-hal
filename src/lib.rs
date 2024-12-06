65e2cd55ca20a97598e6a8c6e068e2c2dfa62d8c
Albin Hedman <albin9604@gmail.com>
2024-12-06 15:40:03 +0100

    Dont refer to non existing feature

--- src/lib.rs
+++ src/lib.rs
@@ -111,8 +111,6 @@ pub use stm32h7::stm32h753v as stm32;
 // Dual core
 #[cfg(feature = "stm32h747cm7")]
 pub use stm32h7::stm32h747cm7 as stm32;
-#[cfg(feature = "stm32h757cm7")]
-pub use stm32h7::stm32h757cm7 as stm32;
 // TODO(rm0399): soundness of PeripheralREC macro in rcc/rec.rs
 
 // High Memory Integration
